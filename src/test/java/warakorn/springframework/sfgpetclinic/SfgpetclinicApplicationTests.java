59c7acd884029bcecbc2b738d7d65df44ab73a88
neugndestiny <neungdestiny12@gmail.com>
2018-08-16 14:55:25 +0700

    Create POJO Model

---
+++ src/test/java/warakorn/springframework/sfgpetclinic/SfgpetclinicApplicationTests.java
@@ -0,0 +1,16 @@
+package warakorn.springframework.sfgpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfgpetclinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
