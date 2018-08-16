0a48788dcad53174145a8ce6b58d464465157843
neugndestiny <neungdestiny12@gmail.com>
2018-08-16 15:38:43 +0700

    Separate the modules

--- petclinicweb/src/main/java/warakorn/framework/sfgpetclinic/SfgpetclinicApplication.java
+++ petclinicweb/src/main/java/warakorn/framework/sfgpetclinic/SfgpetclinicApplication.java
@@ -1,4 +1,4 @@
-package warakorn.springframework.sfgpetclinic;
+package warakorn.framework.sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- petclinicweb/src/test/java/sfgpetclinic/SfgpetclinicApplicationTests.java
+++ petclinicweb/src/test/java/sfgpetclinic/SfgpetclinicApplicationTests.java
@@ -1,4 +1,4 @@
-package warakorn.springframework.sfgpetclinic;
+package sfgpetclinic;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
