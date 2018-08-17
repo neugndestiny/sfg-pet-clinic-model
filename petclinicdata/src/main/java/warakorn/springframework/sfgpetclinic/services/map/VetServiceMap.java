9949d0af50f50b76c7fe9a9d17ddaec29d1b2d41
neugndestiny <neungdestiny12@gmail.com>
2018-08-17 13:34:26 +0700

    Create Loading file

---
+++ petclinicdata/src/main/java/warakorn/springframework/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package warakorn.springframework.sfgpetclinic.services.map;
+
+import warakorn.springframework.sfgpetclinic.model.Vet;
+import warakorn.springframework.sfgpetclinic.services.CrudService;
+import warakorn.springframework.sfgpetclinic.services.VetService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService{
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ petclinicweb/src/main/java/warakorn/framework/sfgpetclinic/bootstrap/DataLoader.java
@@ -0,0 +1,57 @@
+package warakorn.framework.sfgpetclinic.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import warakorn.springframework.sfgpetclinic.model.Owner;
+import warakorn.springframework.sfgpetclinic.model.Vet;
+import warakorn.springframework.sfgpetclinic.services.OwnerService;
+import warakorn.springframework.sfgpetclinic.services.VetService;
+import warakorn.springframework.sfgpetclinic.services.map.OwnerServiceMap;
+import warakorn.springframework.sfgpetclinic.services.map.VetServiceMap;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    public DataLoader() {
+        ownerService = new OwnerServiceMap();
+        vetService = new VetServiceMap();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("Neung");
+        owner1.setLastName("Warakorn");
+
+        ownerService.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstName("M");
+        owner2.setLastName("-150");
+
+        ownerService.save(owner2);
+
+        System.out.println("Loading Owners...");
+
+        Vet vet1 = new Vet();
+        vet1.setId(3L);
+        vet1.setFirstName("Dog");
+        vet1.setLastName("Doggo");
+
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setId(3L);
+        vet2.setFirstName("Cat");
+        vet2.setLastName("Catty");
+
+        vetService.save(vet2);
+
+        System.out.println("Loading Vets...");
+    }
+}
