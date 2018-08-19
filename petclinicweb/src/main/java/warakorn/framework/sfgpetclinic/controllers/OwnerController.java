5faee6b9ff877ea46171fbe1c6bf555c8522ed79
neugndestiny <neungdestiny12@gmail.com>
2018-08-19 09:34:42 +0700

    Add @ComponentScan annotation fixed "No beans of ... type found" and create owner template

--- petclinicweb/src/main/java/warakorn/framework/sfgpetclinic/controllers/OwnerController.java
+++ petclinicweb/src/main/java/warakorn/framework/sfgpetclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package warakorn.framework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import warakorn.springframework.sfgpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/","/index","/index.html"})
-    public String listOwner() {
+    public String listOwner(Model model) {
+        System.out.println(ownerService.findAll());
+        model.addAttribute("owners", ownerService.findAll());
+
         return "owners/index";
     }
 }
