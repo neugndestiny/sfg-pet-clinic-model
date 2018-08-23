fb6419c6c87bb326cc7c69c9eb9a375df087dd1b
neugndestiny <neungdestiny12@gmail.com>
2018-08-23 17:57:15 +0700

    Implement map impl for Visit Service

---
+++ petclinicdata/src/main/java/warakorn/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package warakorn.springframework.sfgpetclinic.services;
+
+import warakorn.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
