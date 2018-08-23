27a8e0455bfde3166376b2ae5a22bc15e2f9d25f
neugndestiny <neungdestiny12@gmail.com>
2018-08-23 16:18:52 +0700

    Convert Vets to JPA Entities by assign @Column above variables

--- petclinicdata/src/main/java/warakorn/springframework/sfgpetclinic/model/Vet.java
+++ petclinicdata/src/main/java/warakorn/springframework/sfgpetclinic/model/Vet.java
@@ -1,9 +1,16 @@
 package warakorn.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
+
+    @ManyToMany(fetch = FetchType.EAGER)    //Jpa load everything all at once
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
